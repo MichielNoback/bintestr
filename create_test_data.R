59c3a222331441fd4d449d7e5de1e79c72e6e207
Michiel <m.a.noback@pl.hanze.nl>
2018-09-18 15:55:50 +0200

    minor changes

--- create_test_data.R
+++ create_test_data.R
@@ -1,16 +1,15 @@
 data.folder <- "external_data"
 
+create.snake.df <- function() {
+    data.file <- "snakes.csv"
+    path <- file.path(data.folder, data.file)
+    snakes <- read.table(path, sep = ";", header = T)
+    devtools::use_data(snakes, overwrite = TRUE)
+}
 
 create.test.data <- function() {
-    create.snake.sizes()
+    create.snake.df()
 }
 
 create.test.data()
 
-create.snake.sizes <- function() {
-    data.file <- "snakes.csv"
-    path <- file.path(data.folder, file)
-    snakes <- read.table(file.path(data.folder, file), sep = ";", header = T)
-    #head(snakes)
-    devtools::use_data(snakes)
-}
--- external_data/README.md
+++ external_data/README.md
@@ -2,7 +2,8 @@
 
 
 - `snakes` Length–mass allometry in snakes (Feldman and Meiri) Biological Journal of the Linnean Society, Volume 108, Issue 1, 1 January 2013, Pages 161–172 https://academic.oup.com/biolinnean/article/108/1/161/2415670#supplementary-data
-    ```
+
+```
 LENGTH AND MASS DATA FOR SPECIES
 Data for length (mm) and mass (g) arranged by type of measure and by family (data for species with no
 exact citation are personal communication data)
